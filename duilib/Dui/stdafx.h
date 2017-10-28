40f1a9bfb34a9c29de66700374d5f645e483b919
tyrion <tyrion.marting@hotmail.com>
2017-10-28 14:47:52 +0800

    Duilib源码编译通过

---
+++ duilib/Dui/stdafx.h
@@ -0,0 +1,46 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
+// Windows 头文件: 
+#include <windows.h>
+
+// TODO:  在此处引用程序需要的其他头文件
+//from duilb source
+
+#define _CRT_SECURE_NO_DEPRECATE
+
+// Remove pointless warning messages
+#ifdef _MSC_VER
+#pragma warning (disable : 4511) // copy operator could not be generated
+#pragma warning (disable : 4512) // assignment operator could not be generated
+#pragma warning (disable : 4702) // unreachable code (bugs in Microsoft's STL)
+#pragma warning (disable : 4786) // identifier was truncated
+#pragma warning (disable : 4996) // function or variable may be unsafe (deprecated)
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // eliminate deprecation warnings for VS2005
+#endif
+#endif // _MSC_VER
+#ifdef __BORLANDC__
+#pragma option -w-8027		   // function not expanded inline
+#endif
+
+// Required for VS 2008 (fails on XP and Win2000 without this fix)
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0500
+#endif
+
+#include "UIlib.h"
+
+#include <olectl.h>
+
+#define lengthof(x) (sizeof(x)/sizeof(*x))
+#define MAX max
+#define MIN min
+#define CLAMP(x,a,b) (MIN(b,MAX(a,x)))
